e7d3ea3f6e0c80fb8f65b22b3c0523587b334656
Tife <boluwatifejyde1@gmail.com>
2022-08-27 16:56:02 +0800

    First commit

---
+++ app.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 27 16:27:17 2022
+
+@author: Tife
+"""
+
+import pandas as pd
+import streamlit as st
+
+st.title('Hello Streamlit')
+st.subheader('Welcome to my streamlit app')
